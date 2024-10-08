7d8b57330e3b0f1f1ffdf5bc16f9d07538d50987
Deepak <alucard.hellsing1751@gmail.com>
2024-10-08 18:03:59 +0530

    model trained

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from CNNclassifier import logger
 from CNNclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNNclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from CNNclassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 
@@ -28,6 +28,21 @@ try:
     obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+STAGE_NAME = "Training Stage"
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
